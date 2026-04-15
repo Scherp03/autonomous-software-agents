c0ce082360d2818d5721a0731ab6583ef43bc1eb
Scherp03 <luca.carpella@gmail.com>
2026-04-15 22:39:21 +0200

    first logic of bdi agent, need to arrange into files

---
+++ random_agent.js
@@ -0,0 +1,61 @@
+import 'dotenv/config';
+import { DjsConnect } from "@unitn-asa/deliveroo-js-sdk/client";
+
+const socket = DjsConnect();
+
+async function agentLoop () {
+
+    var previous = 'right'
+
+    while ( true ) {
+
+        await socket.emitPutdown();
+
+        await socket.emitPickup();
+
+        /** @type {string[]} */
+        let tried = [];
+
+        while ( tried.length < 4 ) {
+            
+            let current = { up: 'down', right: 'left', down: 'up', left: 'right' }[previous] // backward
+
+            if ( tried.length < 3 ) { // try haed or turn (before going backward)
+                current = [ 'up', 'right', 'down', 'left' ].filter( d => d != current )[ Math.floor(Math.random()*3) ];
+            }
+            
+            if ( current && ! tried.includes(current) ) {
+                
+                if ( await socket.emitMove( current ) ) {
+                    console.log( 'moved', current );
+                    previous = current;
+                    break; // moved, continue
+                }
+                
+                tried.push( current );
+                
+            }
+            
+        }
+
+        if ( tried.length == 4 ) {
+            console.log( 'stucked' );
+            await new Promise(res=>setTimeout(res,1000)); // stucked, wait 1 sec and retry
+        }
+
+
+    }
+}
+
+agentLoop()
+
+
+
+const oldLog = console.log;
+/**
+ * @param  {...any} message 
+ */
+global.console.log = function ( ...message ) {
+    socket.emitLog( ...message );
+    oldLog.apply( console, message );
+};
